fc904eda78b7b3bd42aad37ce82fdccf8d79e5c7
Marek Nowakowski <marsznow@gmail.com>
2021-07-03 14:06:46 +0200

    Score manager

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class ScoreManager : MonoBehaviour
+{
+    private float currentScore = 0;
+    private Text scoreText;
+
+    private TimeSpan timeSpan;
+
+    private void Start()
+    {
+        scoreText = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        currentScore += Time.deltaTime;
+        UpdateScoreText();
+    }
+
+    private void UpdateScoreText()
+    {
+        timeSpan = TimeSpan.FromSeconds(currentScore);
+        scoreText.text = timeSpan.ToString("mm':'ss");
+    }
+
+    public float GetScore()
+    {
+        return currentScore;
+    }
+}
