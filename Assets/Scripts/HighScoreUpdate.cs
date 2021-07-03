adfb04951edfe7468103f7c94a3f654a2eb8d884
Marek Nowakowski <marsznow@gmail.com>
2021-07-03 13:38:17 +0200

    Main menu set up

---
+++ Assets/Scripts/HighScoreUpdate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+[RequireComponent(typeof(Text))]
+public class HighScoreUpdate : MonoBehaviour
+{
+    private const string HIGH_SCORE_KEY = "HighScore";
+    private Text highScoreText;
+
+    private void Start()
+    {
+        highScoreText = GetComponent<Text>();
+        UpdateHighScoreText();
+    }
+
+    private void UpdateHighScoreText()
+    {
+        float highScore = PlayerPrefs.GetFloat(HIGH_SCORE_KEY, 0);
+        TimeSpan highScoreTimeSpan = TimeSpan.FromSeconds(highScore);
+        highScoreText.text = highScoreTimeSpan.ToString("mm':'ss");
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private const string GAME_SCENE_NAME = "GameScene";
+    private const string MENU_SCENE_NAME = "MenuScene";
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene(GAME_SCENE_NAME);
+    }
+
+    public void LoadMenuScene()
+    {
+        SceneManager.LoadScene(MENU_SCENE_NAME);
+    }
+}
